5a360ce18d95bc19bb4b577bfe32b2421ca9a38a
Matheus Leandro <mattleandroo@outlook.com>
2020-01-18 15:19:54 -0300

    change in pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import SignIn from '../pages/SingIn';
-import SignUp from '../pages/SingUp';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
@@ -10,11 +10,11 @@ import Profile from '../pages/Profile';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact componet={SignIn} />
-      <Route path="/register" componet={SignUp} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" componet={Dashboard} />
-      <Route path="/profile" componet={Profile} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   );
 }
